7e48d0c860de2a9d8fd96221c0689229aa55b8da
許郁彬 <yubin@xuyubinde-MacBook-Air.local>
2018-06-16 18:29:53 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+var mq = $('.marquee').marquee({
+    //speed in milliseconds of the marquee
+    duration: 10 * 1000,
+    //gap in pixels between the tickers
+    gap: 50,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 0,
+    //'left' or 'right'
+    direction: 'up',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: true
+});

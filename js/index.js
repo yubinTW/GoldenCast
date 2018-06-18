7f4dde9c408860b5e703de9fc981f192d5f5b75b
Administrator <admin@example.com>
2018-06-18 17:11:31 +0800

    add last year message

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 
 var mq = $('.marquee').marquee({
     //speed in milliseconds of the marquee
-    duration: 10 * 1000,
+    duration: 26 * 1000,
     //gap in pixels between the tickers
-    gap: 50,
+    gap: 500,
     //time in milliseconds before the marquee will start animating
     delayBeforeStart: 0,
     //'left' or 'right'
     direction: 'up',
     //true or false - should the marquee be duplicated to show an effect of continues flow
-    duplicated: false
+    duplicated: true
 });
